b0fc6f6f110c71187b8bdfee15b8383f68bb1113
Ivan Ferrera <ivanferrera@gmail.com>
2024-03-25 11:22:09 -0400

    Add custom image domains and disable X-Powered-By header in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ziti.io',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'profile.ziti.io',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/**'
+      }
+    ]
+  },
+  poweredByHeader: false
+}
 
-export default nextConfig;
+export default nextConfig
