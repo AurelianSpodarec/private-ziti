0ab75b3eed6766ee2c57412e254b08de48af876e
Ivan Ferrera <ivanferrera@gmail.com>
2024-03-25 14:31:06 -0400

    tailwind config theme and color updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,48 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
+  prefix: '',
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        xs: '320px', // iPhone SE, Samsung Galaxy A10
+        sm: '640px', // iPhone 8, Samsung Galaxy S8
+        md: '768px', // iPad, Samsung Galaxy Tab S6
+        lg: '1024px', // iPad Pro, small laptops
+        xl: '1280px', // Medium laptops
+        '2xl': '1536px' // Larger laptops, like MacBook Pro 16"
+      }
     },
+    fontFamily: {
+      serifpro: ['source-serif-pro'],
+      poppins: ['Poppins']
+    },
+    extend: {
+      colors: {
+        // Define custom color names or override existing
+        primary: '#3490dc', // Example color
+        secondary: '#ffed4a', // Example color
+        // Define dark theme specific colors
+        dark: {
+          background: '#1a202c',
+          text: '#f7fafc',
+          // more dark theme colors...
+        },
+        // Define light theme specific colors
+        light: {
+          background: '#ffffff',
+          text: '#1a202c',
+          // more light theme colors...
+        },
+      },
+    }
   },
   plugins: [],
 };
