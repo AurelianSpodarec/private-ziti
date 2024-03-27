fb1752a4f9d158c1fe3cbbd26301a42a603ecc2d
Ivan Ferrera <ivanferrera@gmail.com>
2024-03-27 00:19:21 -0400

    add mixpanel initializer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import MixpanelInitializer from '../components/MixpanelInitializer';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <MixpanelInitializer />
       <body className={inter.className}>{children}</body>
     </html>
   );
