749e2c8247b6d7cc3dd4780e44f5a06d275f8734
Ivan Ferrera <ivanferrera@gmail.com>
2024-03-28 18:14:00 -0400

    eslint refactoring

--- src/lib/utility/mixpanel.ts
+++ src/lib/utility/mixpanel.ts
@@ -1,17 +1,17 @@
 // src/lib/utility/mixpanel.ts
 
-import mixpanel from 'mixpanel-browser';
+import mixpanel from 'mixpanel-browser'
 
 // Function to initialize Mixpanel with token from environment variable
-export function initializeMixpanel(): void {
-  const token = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
-  if (!token) {
-    console.warn('NEXT_PUBLIC_MIXPANEL_TOKEN is not set in the environment. Mixpanel has not been initialized.');
-    return;
+export function initializeMixpanel (): void {
+  const token = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN
+  if (token === undefined) {
+    console.warn('NEXT_PUBLIC_MIXPANEL_TOKEN is not set in the environment. Mixpanel has not been initialized.')
+    return
   }
-  
-  mixpanel.init(token, { debug: true, track_pageview: true, persistence: 'localStorage' });
+
+  mixpanel.init(token, { debug: true, track_pageview: true, persistence: 'localStorage' })
 }
 
 // Export mixpanel to be used by src/app/components/MixpanelInitializer.tsx
-export default mixpanel;
+export default mixpanel
