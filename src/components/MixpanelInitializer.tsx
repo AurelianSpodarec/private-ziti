0525d2e9577dcb5785c2392d42800a8b271ebb04
Ivan Ferrera <ivanferrera@gmail.com>
2024-03-28 23:24:51 -0400

    refactored

--- src/components/MixpanelInitializer.tsx
+++ src/components/MixpanelInitializer.tsx
@@ -2,16 +2,34 @@
 
 'use client'
 
-import type React from 'react'
 import { useEffect } from 'react'
-import { initializeMixpanel } from '@/lib/utility/mixpanel'
+import mixpanel from 'mixpanel-browser'
 
-const MixpanelInitializer: React.FC = () => {
+const MixpanelInitializer = (): null => {
   useEffect(() => {
-    initializeMixpanel()
+    // Only initialize Mixpanel if in production environment
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('Mixpanel initialization skipped outside of production environment.')
+      return
+    }
+
+    const token = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN
+    if (token === undefined) {
+      console.warn('NEXT_PUBLIC_MIXPANEL_TOKEN is not set in the environment. Mixpanel has not been initialized.')
+      return
+    }
+
+    mixpanel.init(token, {
+      api_host: 'https://ziti.io/mixpanel',
+      debug: false,
+      track_pageview: true,
+      persistence: 'localStorage'
+    })
+
+    // Additional Mixpanel configuration or event tracking can be added here
   }, [])
 
-  return null
+  return null // This component does not render anything
 }
 
 export default MixpanelInitializer
