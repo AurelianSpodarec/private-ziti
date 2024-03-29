f2c687d8e258b4008de913554c61dd52b290ecf6
Ivan Ferrera <ivanferrera@gmail.com>
2024-03-28 21:25:56 -0400

    refactor - only init mixpanel in production

--- src/lib/utility/mixpanel.ts
+++ src/lib/utility/mixpanel.ts
@@ -2,16 +2,27 @@
 
 import mixpanel from 'mixpanel-browser'
 
-// Function to initialize Mixpanel with token from environment variable
 export function initializeMixpanel (): void {
+  // Only initialize Mixpanel if in production environment
+  if (process.env.NODE_ENV !== 'production') {
+    console.log('Mixpanel initialization skipped outside of production environment.')
+    return
+  }
+
   const token = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN
   if (token === undefined) {
     console.warn('NEXT_PUBLIC_MIXPANEL_TOKEN is not set in the environment. Mixpanel has not been initialized.')
     return
   }
 
-  mixpanel.init(token, { api_host: 'https://ziti.io/mixpanel', debug: process.env.NODE_ENV !== 'production', track_pageview: true, persistence: 'localStorage' })
+  mixpanel.init(token, {
+    api_host: 'https://ziti.io/mixpanel',
+    debug: false,
+    track_pageview: true,
+    persistence: 'localStorage'
+  })
+
+  console.log('Mixpanel has been initialized.')
 }
 
-// Export mixpanel to be used by src/app/components/MixpanelInitializer.tsx
 export default mixpanel
