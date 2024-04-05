d330b39ffc49477527bbc56f13ae99fc9c9e4101
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-04-05 21:48:18 +0200

    feat: Added basic UIKit view to showcase all components

---
+++ src/app/uikit/avatar/page.tsx
@@ -0,0 +1,14 @@
+import Avatar from "@/components/atoms/Avatar"
+
+function UIKitAvatar() {
+  return (
+    <div>
+      
+      
+      <Avatar />
+      <Avatar verified />
+    </div>
+  )
+}
+
+export default UIKitAvatar
