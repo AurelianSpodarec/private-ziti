3c967ff6076dd6270f212f7596895fce9bf052dc
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-04-05 04:18:23 +0200

    feat: Added fetching for single property

--- src/services/apis/requests/listing/properties.tsx
+++ src/services/apis/requests/listing/properties.tsx
@@ -10,3 +10,7 @@ import { IProperty } from "@/interfaces/IProperties"
 export async function getPropertiesList(): Promise<IProperty[]> {
     return FetchZiti(`properties`, "GET")
 }
+
+export async function getPropertySingle(id:string): Promise<IProperty> {
+    return FetchZiti(`properties/${id}`, "GET")
+}
