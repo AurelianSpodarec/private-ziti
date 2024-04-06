79e1228f67a6ada624bbbdee989d86f6462eda02
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-04-06 16:37:54 +0200

    feat: Made a working carousel

--- src/components/organisms/CardProperty.tsx
+++ src/components/organisms/CardProperty.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 import { IProperty } from "@/interfaces/IProperties";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+
+function CarouselProperty() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
 
 function CardProperty({ isLoading, data }: { isLoading?: boolean, data?: IProperty }) {
-
-  if(isLoading) {
+  if (isLoading) {
     return (
       <article>
         IsLoading
@@ -12,22 +29,62 @@ function CardProperty({ isLoading, data }: { isLoading?: boolean, data?: IProper
   }
   return (
     <article className="relative isolate flex flex-col overflow-hidden h-[330px] rounded-2xl p-4">
-      <Link href={`properties/${data?.id}`}>
-        <img
+      <Link href={`properties/${data?.id}`} className="h-full flex flex-col">
+
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem className="px-0 mx-0">
+              <img
+                src="https://s3-alpha-sig.figma.com/img/4790/7b63/d9692c979f33c0eb3b2278fe275605b7?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HCLGkOfHA-eYSMoNjQVY~uZyeqPR0nKC6BxYXPLcH57UMsG2BQ8JZS8SGbLMktw9G10K8JmkpJ5slwRdeSB0aNAf1TVQi1cil68BiWSafyorttukJtrN~oMDxA~CGde1jy36BwyV7sFl-kaXGGme3qwGZzDNoFAWmZKkKYVbszqk7omjnHLdRSZJ6GuvCTfGaHRCPUHT6UAlrBaV8PqLnvKicd0NOykPikJWOIMdOeyNKQJo4XJGIr03wXCm54NRg5BNcOYNY~-bj8htxBSl8ecXyYFvSAWuCRNhhzQFztuWEjGvQKOBqZDyWnE~2m5qShmbfuZLZEUnshlurV0GHQ__"
+                alt=""
+                className="h-full w-full object-cover"
+              />
+            </CarouselItem>
+            <CarouselItem className="px-0 mx-0">
+              <img
+                src="https://s3-alpha-sig.figma.com/img/4790/7b63/d9692c979f33c0eb3b2278fe275605b7?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HCLGkOfHA-eYSMoNjQVY~uZyeqPR0nKC6BxYXPLcH57UMsG2BQ8JZS8SGbLMktw9G10K8JmkpJ5slwRdeSB0aNAf1TVQi1cil68BiWSafyorttukJtrN~oMDxA~CGde1jy36BwyV7sFl-kaXGGme3qwGZzDNoFAWmZKkKYVbszqk7omjnHLdRSZJ6GuvCTfGaHRCPUHT6UAlrBaV8PqLnvKicd0NOykPikJWOIMdOeyNKQJo4XJGIr03wXCm54NRg5BNcOYNY~-bj8htxBSl8ecXyYFvSAWuCRNhhzQFztuWEjGvQKOBqZDyWnE~2m5qShmbfuZLZEUnshlurV0GHQ__"
+                alt=""
+                className="h-full w-full object-cover"
+              />
+            </CarouselItem>
+            <CarouselItem className="px-0 mx-0">
+              <img
+                src="https://s3-alpha-sig.figma.com/img/4790/7b63/d9692c979f33c0eb3b2278fe275605b7?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HCLGkOfHA-eYSMoNjQVY~uZyeqPR0nKC6BxYXPLcH57UMsG2BQ8JZS8SGbLMktw9G10K8JmkpJ5slwRdeSB0aNAf1TVQi1cil68BiWSafyorttukJtrN~oMDxA~CGde1jy36BwyV7sFl-kaXGGme3qwGZzDNoFAWmZKkKYVbszqk7omjnHLdRSZJ6GuvCTfGaHRCPUHT6UAlrBaV8PqLnvKicd0NOykPikJWOIMdOeyNKQJo4XJGIr03wXCm54NRg5BNcOYNY~-bj8htxBSl8ecXyYFvSAWuCRNhhzQFztuWEjGvQKOBqZDyWnE~2m5qShmbfuZLZEUnshlurV0GHQ__"
+                alt=""
+                className="h-full w-full object-cover"
+              />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        {/* <img
           src="https://s3-alpha-sig.figma.com/img/4790/7b63/d9692c979f33c0eb3b2278fe275605b7?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HCLGkOfHA-eYSMoNjQVY~uZyeqPR0nKC6BxYXPLcH57UMsG2BQ8JZS8SGbLMktw9G10K8JmkpJ5slwRdeSB0aNAf1TVQi1cil68BiWSafyorttukJtrN~oMDxA~CGde1jy36BwyV7sFl-kaXGGme3qwGZzDNoFAWmZKkKYVbszqk7omjnHLdRSZJ6GuvCTfGaHRCPUHT6UAlrBaV8PqLnvKicd0NOykPikJWOIMdOeyNKQJo4XJGIr03wXCm54NRg5BNcOYNY~-bj8htxBSl8ecXyYFvSAWuCRNhhzQFztuWEjGvQKOBqZDyWnE~2m5qShmbfuZLZEUnshlurV0GHQ__"
           alt=""
           className="absolute inset-0 -z-10 h-full w-full object-cover"
-        />
-        <div>
-          <div>{data?.Sector.name}</div>
+        /> */}
+
+
+
+
+        <div className="flex items-center justify-between">
+          <div>New</div>
+          <button>Like</button>
         </div>
-        <header className="bg-white rounded-3xl mt-auto p-4">
-          <div className="flex justify-between items-center">
-            <h3>{data?.title}</h3>
-            <span>{data?.Currency}{data?.price}</span>
+
+        <div className="mt-auto">
+          <div>
+            <div>{data?.Sector.name}</div>
           </div>
-          <p>{data?.description}</p>
-        </header>
+
+          <header className="bg-white rounded-3xl mt-auto p-4">
+            <div className="flex justify-between items-center">
+              <h3>{data?.title}</h3>
+              <span>{data?.Currency}{data?.price}</span>
+            </div>
+            <p>{data?.description}</p>
+          </header>
+        </div>
       </Link>
     </article>
   )
