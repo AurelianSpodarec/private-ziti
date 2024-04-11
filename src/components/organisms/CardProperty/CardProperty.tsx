441e983c5c02204b5bbaa98dd3be6023e7b8feb6
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-04-11 21:29:50 +0200

    fix: Removed TypeScript errors and added some more types for CardProperty

--- src/components/organisms/CardProperty/CardProperty.tsx
+++ src/components/organisms/CardProperty/CardProperty.tsx
@@ -1,48 +1,29 @@
 import Link from "next/link";
-import { IProperty } from "@/interfaces/IProperties";
+
+import { IProperty, IPropertyStatus } from "@/interfaces/IProperties";
 import CarouselProperty from "./_components/PropertyCarousel";
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button/button";
 import Skeleton from "@/components/atoms/Skeleton";
 
-// TODO: Remove once the images are inside the database; used temporarly to simulate the images
-const images = [
-  "https://mygate.com/wp-content/uploads/2023/07/110.jpg",
-  "https://images.unsplash.com/photo-1564013799919-ab600027ffc6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwaG91c2V8ZW58MHx8MHx8fDA%3D",
-  "https://s3-alpha-sig.figma.com/img/4790/7b63/d9692c979f33c0eb3b2278fe275605b7?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HCLGkOfHA-eYSMoNjQVY~uZyeqPR0nKC6BxYXPLcH57UMsG2BQ8JZS8SGbLMktw9G10K8JmkpJ5slwRdeSB0aNAf1TVQi1cil68BiWSafyorttukJtrN~oMDxA~CGde1jy36BwyV7sFl-kaXGGme3qwGZzDNoFAWmZKkKYVbszqk7omjnHLdRSZJ6GuvCTfGaHRCPUHT6UAlrBaV8PqLnvKicd0NOykPikJWOIMdOeyNKQJo4XJGIr03wXCm54NRg5BNcOYNY~-bj8htxBSl8ecXyYFvSAWuCRNhhzQFztuWEjGvQKOBqZDyWnE~2m5qShmbfuZLZEUnshlurV0GHQ__",
-  "https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/wp-content/uploads/2021/08/download-7.jpg",
-  "https://loveincorporated.blob.core.windows.net/contentimages/gallery/574bc9b2-d0e0-43e3-aa71-33287a758b15-f932a3fb-4621-4e69-967c-10151ecf7b28-la-mega-mansion-underground-secret.jpg",
-  "https://metro.co.uk/wp-content/uploads/2019/09/PRI_86925433.jpg?quality=90&strip=all",
-  "https://ap.rdcpix.com/5aa9e4f65840eb123875a248b53ca80dl-m1667282403od-w480_h360.jpg",
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOJsYSkBRcPrU-M3vv4a-YEcVv1IGQJF0upfsh4AnmtOiFRNNmaRdtf9v8HpPJkCZ92k8&usqp=CAU",
-  "https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/c631445b62dd159b3a59ca4ec8c277b4-full.jpg",
-  "https://i.pinimg.com/originals/4d/e1/f8/4de1f83293c5786d62f838387244b485.jpg",
-  "https://media.istockphoto.com/id/1391605300/photo/colorful-orange-residential-home-or-house-in-puerto-rico.jpg?s=612x612&w=0&k=20&c=cI-nRl5IB2IGQ0YwyXXIDO3W0xlRDg-XiAjGHNZ3b3o=",
-  "https://cdn.hometogo.net/large/e_v5/55a/724/ead310553a369da4dff0b2b16f.jpg",
-  "https://www.trulia.com/pictures/thumbs_4/zillowstatic/fp/35865cc9430e430cb537515e9e1bc190-full.jpg",
-  "https://ap.rdcpix.com/35fe40062461880943f95c882a6406a3l-m3114675221od-w480_h360.jpg"
-]
+import { getShuffledArr, images } from "./fakePropertyData";
 
-// TODO: Remove once real data is in the listings
-const getShuffledArr = (arr: [string]) => {
-  const newArr = arr.slice()
-  for (let i = newArr.length - 1; i > 0; i--) {
-    const rand = Math.floor(Math.random() * (i + 1));
-    [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
-  }
-  return newArr
-};
-
-interface ICardProperty {
-  isLoading?: boolean;
+type ICardProperty = {
   data?: IProperty;
-  isNew?: boolean;
-  underConstruction?: boolean;
-  propertyStatus: "available" | "new" | "construction";
+  isLoading?: boolean;
+  propertyStatus: keyof Pick<IPropertyStatus, "statusName">;
 }
 
-function CardProperty({ isLoading, data }: ICardProperty) {
-  
+type ICardPropertyStatuses = {
+  [key in IPropertyStatus['statusName']]: {
+    name: string;
+    class: string;
+  }
+}
+
+function CardProperty({ data, isLoading }: ICardProperty) {
+
   if (isLoading) {
     return (
       <article>
@@ -55,9 +36,9 @@ function CardProperty({ isLoading, data }: ICardProperty) {
       </article>
     )
   }
-  
-  const propertyStatus = data?.PropertyStatus.statusName
-  const propertyStatuses = {
+
+  const cardPropertyStatus = data && data?.PropertyStatus && data?.PropertyStatus.statusName
+  const cardPropertyStatuses: ICardPropertyStatuses = {
     available: {
       name: "",
       class: "",
@@ -85,10 +66,9 @@ function CardProperty({ isLoading, data }: ICardProperty) {
             <div className="absolute w-full z-10">
               <div className="flex items-center justify-between z-10">
 
-                {/* TODO: Add TypeScript */}
                 {data?.PropertyStatus.statusName !== "available" &&
-                  <Badge className={`${propertyStatuses[propertyStatus].class} backdrop-blur-sm bg-opacity-60`}>
-                    {propertyStatuses[propertyStatus].name}
+                  <Badge className={`${cardPropertyStatus && cardPropertyStatuses[cardPropertyStatus].class} backdrop-blur-sm bg-opacity-60`}>
+                    {cardPropertyStatus && cardPropertyStatuses[cardPropertyStatus].name}
                   </Badge>
                 }
                 <Button className="ml-auto rounded-full" variant="clean" kind="glass">
