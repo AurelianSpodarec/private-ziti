0337ff1c682b1b3e03f5bef2e07d4e24c7626150
Ivan Ferrera <ivanferrera@gmail.com>
2024-05-10 19:03:19 -0400

    enable metadata and disallow bot crawling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 
 import MixpanelInitializer from '@/lib/other/MixpanelInitializer'
 import TrafficSourceTracker from '@/lib/other/TrafficSourceTracker'
@@ -14,11 +14,23 @@ import Footer from './_components/Footer'
 
 // import MobileNativeMenu from './_components/MobileNativeMenu'
 
-// export const metadata: Metadata = {
-//   title: 'Discover, Connect, Invest - Ziti',
-//   description: 'Explore global property listings, connect with experts, and invest confidently worldwide with Ziti.',
-//   robots: 'noindex'
-// }
+export const metadata: Metadata = {
+  title: 'Discover, Connect, Invest - Ziti',
+  description: 'Explore global property listings, connect with experts, and invest confidently worldwide with Ziti.',
+  robots: {
+    index: false, // Disallow indexing of all pages
+    follow: false, // Do not follow links from this page
+    nocache: true, // Instructs to not cache the page
+    googleBot: {
+      index: false, // Specifically tell Googlebot not to index
+      follow: false, // Specifically tell Googlebot not to follow links
+      noimageindex: true, // Prevent Googlebot from indexing images
+      'max-video-preview': -1, // No video previews
+      'max-image-preview': 'none', // No image previews
+      'max-snippet': -1 // No snippets
+    }
+  }
+}
 
 function RootLayout ({ children }: Readonly<{ children: React.ReactNode }>): JSX.Element {
   return (
