a51d992dd38b55227551c2c17e53ec551c9d21a2
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-05-10 19:53:41 +0100

    update: Added conditional to NOT render the menu on blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,27 +6,46 @@ import { getArticles } from '@/services/apis/requests/blog'
 import CardArticle from '@/components/organisms/CardArticle'
 import Container from '@/components/Container'
 import CardFeaturedArticle from '@/components/organisms/CardFeaturedArticle'
+import Image from 'next/image'
 
 async function BlogIndex () {
   const res = await getArticles()
   const articles = res.NewsArticles || []
 
   return (
-    <Container size="6xl">
-      {articles.length !== 0 &&
-        <>
-          <div className="mb-16">
-            <CardFeaturedArticle data={articles[0]} />
-          </div>
+    <>
+      <title>Blog - Ziti</title>
 
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
-            {articles.slice(1).map((item) => (
-              <CardArticle key={item.id} data={item} />
-            ))}
+      <section className="bg-oceanicForest-800">
+        <Container>
+          <div>
+            <h1>Your Source for Real Estate Insights and Inspiration</h1>
+            <p>Discover Expert Tips, Market Trends, and Stories From the Dominican Republic Real Estate Scene</p>
           </div>
-        </>
-      }
-    </Container>
+          <div>
+            <div className="relative w-full h-full">
+              <Image src="/images/blog-image-banner.png" alt="Real Estate Homes" fill />
+            </div>
+          </div>
+        </Container>
+      </section>
+
+      <Container size="6xl">
+        {articles.length !== 0 &&
+          <>
+            <div className="mb-16">
+              <CardFeaturedArticle data={articles[0]} />
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
+              {articles.slice(1).map((item) => (
+                <CardArticle key={item.id} data={item} />
+              ))}
+            </div>
+          </>
+        }
+      </Container>
+    </>
   )
 }
 
