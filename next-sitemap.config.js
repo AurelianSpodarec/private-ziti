a6ca4a9b214c6b1c3611fc59695efde33fc1166c
Ivan Ferrera <ivanferrera@gmail.com>
2024-05-11 11:56:29 -0400

    Add specific paths to the exclusion list in the sitemap configuration to refine SEO management

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,3 +1,5 @@
+// next-sitemap.config.js
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL !== undefined && process.env.SITE_URL !== null && process.env.SITE_URL !== '' ? process.env.SITE_URL : 'https://ziti.io',
@@ -9,6 +11,21 @@ module.exports = {
         disallow: '/'
       }
     ]
-  }
-  // exclude: ["/exclude"],
+  },
+  exclude: [
+    '/about',
+    '/properties',
+    '/community-guidelines',
+    '/pricing',
+    '/help',
+    '/uikit',
+    '/uikit/*',
+    '/careers',
+    '/privacy-policy',
+    '/testimonials',
+    '/brand-guide',
+    '/faq',
+    '/contact',
+    '/sitemap'
+  ]
 }
