e7b0ea31e14c1e13d366bdd3fd9e90aaf5fadf1b
Ivan Ferrera <ivanferrera@gmail.com>
2024-05-12 18:31:31 -0400

    nocache false

--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -2,10 +2,17 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://ziti.io'),
-  title: 'Discover, Connect, Invest - Ziti',
-  description: 'Explore global property listings, connect with experts, and invest confidently worldwide with Ziti.',
   robots: {
-    nocache: false // Cache the page
+    index: true, // Allow indexing
+    follow: true, // Follow links
+    nocache: false, // Cache the page
+    googleBot: {
+      index: true, // Specifically tell Googlebot to index
+      follow: true, // Specifically tell Googlebot to follow links
+      noimageindex: true, // Prevent Googlebot from indexing images
+      'max-video-preview': -1, // No video previews
+      'max-image-preview': 'none' // No image previews
+    }
   }
 }
 
