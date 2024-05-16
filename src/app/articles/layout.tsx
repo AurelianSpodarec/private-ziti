23be4165191fd322e72dda2134d51250770946ae
Ivan Ferrera <ivanferrera@gmail.com>
2024-05-16 09:18:04 -0400

    check for is_actual_production

--- src/app/articles/layout.tsx
+++ src/app/articles/layout.tsx
@@ -1,20 +1,30 @@
 import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  metadataBase: new URL('https://ziti.io'),
-  robots: {
-    index: true, // Allow indexing
-    follow: true, // Follow links
-    nocache: false, // Cache the page
-    googleBot: {
-      index: true, // Specifically tell Googlebot to index
-      follow: true, // Specifically tell Googlebot to follow links
-      noimageindex: true, // Prevent Googlebot from indexing images
-      'max-video-preview': -1, // No video previews
-      'max-image-preview': 'none' // No image previews
+const isActualProduction = process.env.IS_ACTUAL_PRODUCTION === 'true'
+
+export const metadata: Metadata = isActualProduction
+  ? { // production metadata
+      metadataBase: new URL('https://ziti.io'),
+      robots: {
+        index: true, // Allow indexing
+        follow: true, // Follow links
+        nocache: false, // Cache the page
+        googleBot: {
+          index: true, // Specifically tell Googlebot to index
+          follow: true, // Specifically tell Googlebot to follow links
+          noimageindex: true, // Prevent Googlebot from indexing images
+          'max-video-preview': -1, // No video previews
+          'max-image-preview': 'none' // No image previews
+        }
+      }
     }
-  }
-}
+  : { // non-production metadata
+      robots: {
+        index: false,
+        follow: false,
+        nocache: true
+      }
+    }
 
 function RootLayout ({ children }: Readonly<{ children: React.ReactNode }>): JSX.Element {
   return (
