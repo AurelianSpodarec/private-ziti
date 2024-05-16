2da3d5494d6d1bd5ef93014015247c787e5a217f
Ivan Ferrera <ivanferrera@gmail.com>
2024-05-16 13:17:03 -0400

    also import and export blog metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
-import BlogIndex from './articles/page'
+import BlogIndex, { metadata as blogMetadata } from './articles/page'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = blogMetadata
 
 function Home (): JSX.Element {
   return <BlogIndex />
