b9c80e78ce97e7ea85e860cc387531dad32bf25c
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-05-18 21:39:53 +0100

    update: Creating login flow and trying to grab user data and showcasing it in components

--- src/app/_components/Header/MenuDesktop.tsx
+++ src/app/_components/Header/MenuDesktop.tsx
@@ -1,27 +1,42 @@
-'use client'
-
-// import useModal from '@/context/useModal'
 import { Button } from '@/components/ui/button'
 import UserDropdown from '../UserDropdown'
+import { auth } from '@/auth'
+import { serverActionUser } from './serverActionUser'
+import { useEffect, useState } from 'react'
 
 function MenuDesktop () {
+
+  const [user, setUser] = useState({})
+  useEffect(() => {
+    const fetchData = async () => {
+      let user = await serverActionUser()
+      setUser(user)
+    }
+
+    fetchData()
+  }, [])
+
   const isLogged = false
   // const { openModal } = useModal()
-
+  if (!user.id) return
   return (
     <nav className="hidden lg:block text-sm text-white font-montserrat">
       <div className="flex items-center space-x-7">
         {/* <ModeToggle /> */}
 
         <div className="flex items-center justify-between space-x-2">
-          {isLogged &&
+          {user.id &&
             <UserDropdown />
           }
 
           {/* <Button onClick={() => openModal()}> */}
-          <Button>
-            Login Button
-          </Button>
+          {user?.id ?
+            <p className="text-black">Hello Ivan</p>
+            :
+            <Button>
+              Login Button
+            </Button>
+          }
 
         </div>
 
--- src/components/organisms/Modal/Auth/index.tsx
+++ src/components/organisms/Modal/Auth/index.tsx
@@ -8,81 +8,135 @@ import SocialLoginList from './SocialLoginList'
 import DividerWithText from './DividerWithText'
 import { useEffect, useState } from 'react'
 import { serverLoginEmaislAction } from './loginServerAction'
+import { authCheckEmail } from '@/services/apis/requests/auth'
 
+type LoginMethod = "phone" | "email"
 
-// interface IModalAuth {
-//   isOpen: boolean
-//   onClose?: () => void
-// }
+interface IUserAccount {
+  hasAccount: boolean
+  accountComplete: boolean
+  emailVerified: boolean
+  audienceType: string[]
+  privateAccountData: {
+    obfuscatedEmail: string
+  }
+  cookies: string
+}
 
-// Shuld show when user clicks 'like'
-// Should show when user clicks button 'login'
-
-function StartLogin () {
+function EmailLogin ({ onAction }: any) {
   return (
     <div>
       <div>
         Email
         <Input />
-        <Button block>Continue</Button>
+        <Button onClick={onAction} block>Continue</Button>
       </div>
+
       <DividerWithText text="Or continue with" />
       <SocialLoginList />
     </div>
   )
 }
 
-function FinishRegistering () {
+function EmailFinishRegister ({ onAction }: any) {
   return (
     <div>
-      <Button>Agree and continue</Button>
+      <div>
+        <h2>Legal Name</h2>
+        <Input placeholder='First name on ID' />
+        <Input placeholder='Last name on ID' />
+        <span>Make sure this matches the name on your government ID. If you go by another name, you can add a preferred first name.</span>
+      </div>
+
+      <div>
+        <h2>Daate of birth</h2>
+        <Input placeholder="Date of birth" />
+        <span>To sign up, you need to be at least 18. Your birthday won’t be shared with other people who use Airbnb.</span>
+      </div>
+
+      <div>
+        <h2>Contact Info</h2>
+        <Input placeholder="Contact info" value="email@gmail.com" />
+        <span>We'll email you trip confirmations and receipts.</span>
+      </div>
+
+      <div>
+        <h2>Password</h2>
+        <Input placeholder="Password" />
+      </div>
+
+      <Button block>Continue and agree</Button>
     </div>
   )
 }
 
-// type Option = 'email' | 'phone'
-function ModalAuth () {
-  // method
-  // const [option, setOption] = useState<Option>('email')
-  // const [phase, setPhase] = useState('')
-  // Email or Phone can be selected
-  // Phase one or two can be selected
 
-  // TODO: Check if email exists, if it does, login
+function EmailPassword ({ onAction }: any) {
+  return (
+    <div>
+      Password
+      <Input placeholder="Password" type="password" />
+      <Button onClick={onAction} block>Log in</Button>
+      <p>Forgotten your password</p>
+    </div>
+  )
+}
 
-  const [open, setOpen] = useState(true)
-  // function checkEmail () {
-  //   const res = authCheckEmail('aurelianxspodarec@gmail.com')
-  //   console.log('checkEmailg', res)
-  // }
+function EmailLoginView ({ setOpen }) {
+  const [stage, setStage] = useState('emailLogin') //emailLogin, emailRegister, password
 
+  async function checkEmail () {
+    const res = await authCheckEmail('aurelianxspodarec@gmail.com')
+
+    // TESTING
+    // const canLogin = res.accountComplete && res.hasAccount
+    const canLogin = true
+    if (canLogin) {
+      setStage('password')
+    } else {
+      setStage('emailRegister')
+    }
+
+  }
+
+  // TODO: Update to dynamic values, and check the auth.ts file as well
   async function loginEmail () {
-    // TODO: Currently this goes from the Auth as hardcoded values
-    // const res = authLoginByEmail({
-    //   identifier: 'ivanferrera@gmail.com',
-    //   pwd: 'abc123',
-    //   rememberMe: true
-    // })
     const res = await serverLoginEmaislAction()
-    console.log("res", res)
+    // console.log("res", res.accountComplete)
     if (res) {
       setOpen(false)
     }
-    // If login not successfull
-
-    // login successfull, 
-    // setOpen(false)
   }
 
-  // TODO: Hide modal on successful authentication
-  // We can try 'use server on click
+  return (
+    <div>
+
+      {stage === "emailLogin" && <EmailLogin onAction={checkEmail} />}
+      {stage === "emailRegister" && <EmailFinishRegister />}
+      {stage === "password" && <EmailPassword onAction={loginEmail} />}
+    </div>
+  )
+}
+
+function StartLogin ({ loginMethod = "email", setOpen }: { loginMethod?: LoginMethod }) {
+
+  return (
+    <div>
+      <EmailLoginView setOpen={setOpen} />
+    </div>
+  )
+}
+
+
+type Option = 'email' | 'phone'
+function ModalAuth () {
+  const [open, setOpen] = useState(true)
+  const [option, setOption] = useState<Option>('email')
+
   return (
     <Dialog modal open={open}>
       <DialogContent>
-        {/* <button onClick={() => { checkEmail() }}>Check Email</button> */}
-        <button onClick={() => loginEmail()}>Login Email</button>
-        {/* <StartLogin /> */}
-
+        <StartLogin setOpen={setOpen} />
       </DialogContent>
     </Dialog>
   )
