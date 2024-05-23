5df3d3e134c22f3a8d1d7b88446d0b693bf36d63
Ivan Ferrera <ivanferrera@gmail.com>
2024-05-23 12:48:46 -0400

    increase contrast

--- src/components/molecules/EmptyState/UserAvatarBox.tsx
+++ src/components/molecules/EmptyState/UserAvatarBox.tsx
@@ -12,8 +12,8 @@ function UserAvatarBox ({ src, name, fallbackText, subTitle }: IUserAvatarBox) {
     <div className="flex items-center space-x-2">
       <Avatar src={src} name={name} fallbackText={fallbackText} size="h-10 w-10" />
       <div className="flex flex-col">
-        <span className="text-foreground-250">{name}</span>
-        <span className="text-foreground-250">{subTitle}</span>
+        <span className="text-foreground-500">{name}</span>
+        <span className="text-foreground-500">{subTitle}</span>
       </div>
     </div>
   )
